b231365ac791901d0c3ea42c1a28fd1c6670ee8c
querbys26-dot <querbys26@gmail.com>
2026-01-26 19:24:37 -0600

    First version

---
+++ main.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+
+if word in meme_dict:
+    print(meme_dict[word])
+else:
+    print("esa palabra no esta disponible")
